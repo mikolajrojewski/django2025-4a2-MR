53f0a353806775f4871ae6608db98897f9048f41
Mikołaj Rojewski <miko.tcz06@gmail.com>
2025-04-10 08:30:54 +0200

    Cleaned up unused import.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 # Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
